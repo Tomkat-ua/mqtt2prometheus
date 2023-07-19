92098a580306d404ee90b55b6b8f920a4a1231b0
Viacheslav Kostevych <viacheslav.o.kostevych@raiffeisen.ua>
2023-07-19 23:10:33 +0300

    add device config

--- main.py
+++ main.py
@@ -5,50 +5,67 @@ import random
 from paho.mqtt import client as mqtt_client
 from prometheus_client import start_http_server, Gauge
 from time import sleep as sleep
+########### TEST ###################################
+import confuse
+# import argparse
+config = confuse.Configuration('MQTT-Prom', __name__)
+config.set_file('./config.yml')
+# value = config['config'][2]['param1'].get()
+int_value = config['config']['param2'].get(int)
+str_value = config['config']['mqtt']['host'].get()
+print(int_value)
+print(str_value)
 
 ######
-# with open('config.yml', 'r') as file:
-#     config_file = yaml.safe_load(file)
-#     # config_file = yaml.load(file, Loader=yaml.FullLoader)
-#     print(config_file['config']['mqtt'])
-#     print(config_file['config']['mqtt']['host'])
-# class Config
-#
+
 ######Clases -----------------------
 class Device:
-    device_name=''
+    device_name = None
 class Sensor:
-    sensor_name=''
+    sensor_name = None
 class Topic:
-    name=''
-    raw=''
-    data=''
+    name = None
+    raw = None
+    data = None
 
-
+# class Config(object):
+#     def __init__(self):
+#         self.left = None
+#         self.right = None
+#         self.data = None
+#         self.mqtt = None
+#         # self.mqtt.port = None
+#         # self.mqtt.user = None
+#         # self.mqtt.password = None
 
 #### GLOBAL Vars --------------------
-appver = "0.0.1"
+appver = "0.0.3"
 appname = "MQTT to Prometheus metrics extractor"
 appshortname = "MQTT-Prom"
 print(appname + " ver. "+appver)
 env = 'dev' #argv[1]
 
-http_port = ''
+http_port = None
 get_delay = 5
-broker = ''
-broker_port = ''
-username = ''
-password = ''
-topic_pattern = ''
-parsing_index_device = ''
-parsing_index_sensor = ''
-metric_name = ''
-metric_help = ''
-metric_topic = ''
-metric_index = ''
-loging_index = ''
-loging_topic = ''
+broker = None
+broker_port = None
+username = None
+password = None
+topic_pattern = None
+parsing_index_device = None
+parsing_index_sensor = None
+metric_sensor_name = None
+metric_sensor_help = None
+metric_sensor_topic = None
+metric_sensor_index = None
+metric_device_name = None
+metric_device_help = None
+metric_device_topic = None
+metric_device_index = None
+loging_index = None
+loging_topic = None
 client_id = f'python-mqtt-{random.randint(0, 100)}'
+
 
 ####------------------------------------------------
 
@@ -71,10 +88,17 @@ def config_read():
     globals()["parsing_index_device"] = config_file['config']['parsing']['index_device']
     globals()["parsing_index_sensor"] = config_file['config']['parsing']['index_sensor']
     globals()["parsing_index_sensor"] = config_file['config']['parsing']['index_sensor']
-    globals()["metric_name"] = config_file['config']['metric']['name']
-    globals()["metric_help"] = config_file['config']['metric']['help']
-    globals()["metric_topic"] = config_file['config']['metric']['topic']
-    globals()["metric_index"] = config_file['config']['metric']['index']
+
+    globals()["metric_sensor_name"] = config_file['config']['metric']['sensor']['name']
+    globals()["metric_sensor_help"] = config_file['config']['metric']['sensor']['help']
+    globals()["metric_sensor_topic"] = config_file['config']['metric']['sensor']['topic']
+    globals()["metric_sensor_index"] = config_file['config']['metric']['sensor']['index']
+
+    globals()["metric_device_name"] = config_file['config']['metric']['device']['name']
+    globals()["metric_device_help"] = config_file['config']['metric']['device']['help']
+    globals()["metric_device_topic"] = config_file['config']['metric']['device']['topic']
+    globals()["metric_device_index"] = config_file['config']['metric']['device']['index']
+
     globals()["loging_index"] = config_file['config']['loging']['index']
     globals()["loging_topic"] = config_file['config']['loging']['topic']
 
@@ -124,13 +148,14 @@ def subscribe(client: mqtt_client):
         device.device_name = topic.name[parsing_index_device]
 
         if topic.name[loging_index] == loging_topic:
-            logformer(device.device_name +', '+ topic.data)
-        if topic.name[metric_index] == metric_topic:
+            logformer(device.device_name +', '+ topic.data) # if  topic content log only
+        if topic.name[metric_sensor_index] == metric_sensor_topic: # if topic contend - data
             sensor.sensor_name = topic.name[parsing_index_sensor]
             logformer(device.device_name +', ' +sensor.sensor_name + ', ' + topic.data)
-            if check_float(topic.data):
-                METRICA_SENSOR.labels(device.device_name,sensor.sensor_name,'').set(topic.data)
-            else: METRICA_SENSOR.labels(device.device_name,sensor.sensor_name,topic.data).set(0)
+            if check_float(topic.data): # if data - from sensor
+                METRICA_SENSOR.labels(device.device_name,sensor.sensor_name).set(topic.data)
+            else:  #if data - from device
+                METRICA_DEVICE.labels(device.device_name,sensor.sensor_name,topic.data).set(0)
 
     client.subscribe(topic_pattern)
     client.on_message = on_message_data
@@ -143,7 +168,8 @@ def run():
 
 if __name__ == '__main__':
     config_read()
-    METRICA_SENSOR = Gauge(metric_name, metric_help, ['device', 'sensor', 'data'])
+    METRICA_SENSOR = Gauge(metric_sensor_name, metric_sensor_help, ['device', 'sensor'])
+    METRICA_DEVICE = Gauge(metric_device_name, metric_device_help, ['device', 'sensor', 'value'])
     APP_CONFIG.labels(http_port, get_delay, broker, broker_port, topic_pattern).set(1)
     try:
         start_http_server(http_port)
